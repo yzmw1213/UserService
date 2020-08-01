5eea9c94905b107bcc2a476cf146766fb86b8026
AyumuShigeta <faliplvsg@gmail.com>
2020-08-01 15:40:16 +0900

    go-playground/validatorによるバリデーション追加

--- domain/model/blog.go
+++ domain/model/blog.go
@@ -1,13 +1,19 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validate *validator.Validate
 
 // Blog 投稿サービス構造体
 type Blog struct {
-	BlogID    int32 `gorm:"primary_key"`
-	AuthorID  int32
-	Title     string
-	Content   string
+	BlogID    int32  `gorm:"primary_key"`
+	AuthorID  int32  `validate:"required,number"`
+	Title     string `validate:"min=1,max=32"`
+	Content   string `validate:"min=1,max=140"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
